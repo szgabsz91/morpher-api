db832018b2c0e02646b363fda932c3c6f7ef2a5c
Gabor Szabo <szgabsz91@gmail.com>
2020-09-18 16:26:04 +0200

    feat: #21 make the Morpher instance easily configurable

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
 
+morpherSystemPath=${1:-./data/morpher-system.pb}
 morpherApiVersion=$(grep "morpherApiVersion" gradle.properties | cut -d"=" -f2)
-dockerImageVersion=${1:-${morpherApiVersion}}
+dockerImageVersion=${2:-${morpherApiVersion}}
 
-docker build --build-arg morpherApiVersion=$morpherApiVersion -t morpher-api .
+docker build \
+    --build-arg morpherSystemPath=$morpherSystemPath \
+    --build-arg morpherApiVersion=$morpherApiVersion \
+    -t morpher-api \
+    .
 docker tag morpher-api szgabsz91/morpher-api:$dockerImageVersion
 
 echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
